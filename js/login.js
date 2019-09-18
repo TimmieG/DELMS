04b9ea62a43144790fa5e4aca027407a39bfd623
TimmieG <faithfultimmy@yahoo.com>
2019-09-18 08:04:48 +0100

    .

--- js/login.js
+++ js/login.js
@@ -9,3 +9,33 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function Validate(){
+	var userName = $("#name").val();
+	var pass = $("#password").val();
+
+	if (userName == ''){
+		$('.nameErr').html( + " " +"Enter username");
+	
+	}
+	else if(userName.length < 6){
+		$(".nameErr").html("Name must contain atleast 6 letters");
+	}
+	else if (userName){
+		$(".nameErr").html("");
+	}
+	if (pass == ""){
+		$('.passErr').html("Enter password");
+	
+	}
+	else if(pass.length < 8){
+		$(".passErr").html("Password must have atleast 8 letters including numbers and special characters");
+	}
+	else if (pass){
+		$(".passErr").html("");
+	}
+}
+
+// function getInfo(){
+// 	// var username = 
+// }
